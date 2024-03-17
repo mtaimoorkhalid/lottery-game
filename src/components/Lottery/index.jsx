a9afa04a5f980bc47add409cbeabc72bbe57746d
Muhammad Taimoor Khalid <taimoormkh@gmail.com>
2024-03-17 15:03:31 +0500

    complete win lottery logic and add new ticket button

--- src/components/Lottery/index.jsx
+++ src/components/Lottery/index.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
 import "./index.css";
-import { genTicket } from "../../helper/helper";
+import { genTicket, sum } from "../../helper/helper";
 export default function Lottery() {
   const [lottery, setLottery] = useState(genTicket(3));
+
+  const isWinning = sum(lottery);
+  function getLottery() {
+    setLottery(genTicket(3));
+  }
 
   return (
     <div>
@@ -12,6 +17,10 @@ export default function Lottery() {
         <span>{lottery[1]}</span>
         <span>{lottery[2]}</span>
       </div>
+      <button onClick={getLottery} style={{ margin: "10px" }}>
+        Get New Ticket
+      </button>
+      {isWinning === 15 && <h1>Congratulations you won!</h1>}
     </div>
   );
 }
